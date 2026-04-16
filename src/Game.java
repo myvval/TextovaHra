23a621c581beb305a01123a869198292f9f3213b
Jakub Suchánsky <myval99@seznam.cz>
2026-04-16 18:42:09 +0200

    moved part from main to game

--- src/Game.java
+++ src/Game.java
@@ -3,12 +3,37 @@ import java.util.Scanner;
 public class Game {
     private boolean gameLoop = true;
 
-    public Game() {
+
+    public void startGame() {
+        // creates managers for world, enemies, items
+        WorldMap map = new WorldMap();
+        ItemsManager itemManager = new ItemsManager();
+        EnemiesManager enemies = new EnemiesManager();
+
+        // loads enemies, map, items
+        map.loadMap();
+        map.connectRooms();
+        itemManager.loadItems();
+        enemies.loadEnemies();
+
+        // assign enemies, items to rooms
+        for(Item item:itemManager.getItems()) map.returnListOfRoom().get(item.getLocation()).addItem(item);
+        for(Enemy enemy:enemies.getEnemies()) map.returnListOfRoom().get(enemy.getLocation()).addEnemy(enemy);
+        System.out.println("START OF THE GAME");
     }
+    public void game() {}
 
-    ;
 
-
+    public void setPlayerName() {
+        Scanner scanner = new Scanner(System.in);
+        Player player = new Player("NO NAME");
+        String input;
+        do {
+            System.out.println("WHAT YOUR NAME SHOULD BE?");
+            input = scanner.nextLine();
+        } while (input.isBlank());
+        player.setName(input);
+    }
     public void setupGame() {
         WorldMap map = new WorldMap();
         //map.generateTestMap();
